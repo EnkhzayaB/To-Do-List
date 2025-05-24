e8bce332d09b54062a3463ed9e2040aa20538bc7
25LP0861 25LP0861 <25LP0861@pinecones-iMac-2.local>
2025-05-24 16:58:18 +0800

    edit

--- src/mock/UserData.jsx
+++
@@ -1,5 +0,0 @@
-const UserData = [
-  { id: 1, name: "Enkhzaya", status: "72720001", email: "zaya90@gmail.com" },
-];
-
-export default UserData;
--- src/mock/task.js
+++
@@ -1,6 +0,0 @@
-const tasks = [
-  { id: 1, text: "shud ugaah", status: "completed" },
-  { id: 2, text: "mashin awah", status: "active" },
-];
-
-export default tasks;
